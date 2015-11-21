ebfc336233a57b3f68d97fb0a85f0987a1b0a670
Hankyu <jhkmath@gmail.com>
2015-11-22 01:09:11 +0900

    Modify cleveland.py This creates a csv file with 76 labels and its following data.

--- cleveland.py
+++ cleveland.py
@@ -1,19 +1,22 @@
-import string
+import csv
+from create_list import *
+
+csv_output = open("processed_data/cleveland_data_76_attributes.csv", 'wt', newline="")
+writer = csv.writer(csv_output)
 cleveland_data = open("original_data/cleveland_data.txt", "r")
+cleveland = create_list(cleveland_data)
+total_patients = len(cleveland)
+total_labels = len(cleveland[0])
 
-def create_list():
-    patient_list = []
-    item_list = []
-    for line in cleveland_data:
-        if len(item_list) != 0 and item_list[-1] == "name":
-            patient_list.append(item_list)
-            item_list = []
-        for item in line.split():
-            item = item.strip(string.whitespace)
-            item_list.append(item)
-    patient_list.append(item_list)
-    return patient_list
+writer.writerow(["id", "ccf", "age", "sex", "painloc", "painexer", "relrest", "pncaden", "cp", "trestbps",\
+"htn", "chol", "smoke", "cigs", "years", "fbs", "dm", "famhist", "restecg", "ekgmo",\
+"ekgday", "ekgyr", "dig", "prop", "nitr", "pro", "diuretic", "proto", "thaldur", "thaltime"\
+"met", "thalach", "thalrest", "tpeakbps", "tpeakbpd", "dummy", "trestbpd", "exang", "xhypo", "oldpeak"\
+"slope", "rldv5", "rldv5e", "ca", "restckm", "exerckm", "restef", "restwm", "exeref", "exerwm",\
+"thal", "thalsev", "thalpul", "earlobe", "cmo", "cday", "cyr", "num", "lmt", "ladprox",\
+"laddist", "diag", "cxmain", "ramus", "om1", "om2", "rcaprox", "rcadist", "lvx1", "lvx2",\
+"lvx3", "lvx4", "lvf", "cathef", "junk", "name"])
 
-cleveland = create_list()
-# The last patient doesn't 76 attributes. Pop.
-cleveland.pop()
+writer.writerows(cleveland)
+
+csv_output.close()
