3ffa07c2efaa63688d381147bbc96984e9330f05
Hankyu <jhkmath@gmail.com>
2015-11-21 20:45:02 +0900

    Add SVMAccuracy function

--- classify.py
+++ classify.py
@@ -1,7 +1,15 @@
+from sklearn.metrics import accuracy_score
+
 def NBAccuracy(pred, labels_test):
-    """ compute the accuracy of your Naive Bayes classifier """
+    """ compute the accuracy of Naive Bayes classifier """
 
     # return the accuracy on the test data
-    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(pred, labels_test)
+    return accuracy
+
+def SVMAccuracy(pred, labels_test):
+    """ compute the accuracy of Support Vector Machines classifier """
+
+    # return the accuracy on the test data
     accuracy = accuracy_score(pred, labels_test)
     return accuracy
